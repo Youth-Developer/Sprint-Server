6ab475e0c18cd182f21d0a95a60dcb0fadba9901
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-09-30 15:37:24 +0900

    domain resource generate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MemberModule } from './member/member.module';
+import { BoardModule } from './board/board.module';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
-  imports: [],
+  imports: [MemberModule, BoardModule, AnswerModule],
   controllers: [AppController],
   providers: [AppService],
 })
