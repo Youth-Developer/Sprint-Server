c5ff66da960fdb99f00da37d656c5d85b3bf6181
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-10-06 23:12:47 +0900

    Logger에서 host 환경변수 삭제

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { Logger } from '@nestjs/common';
 
 declare const module: any;
 const port = process.env.PORT || 8080;
-const host = process.env.HOST;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
-  Logger.log(`Server is running on http://${host}:${port}`, 'Bootstrap');
+  Logger.log(`Server is running on http://localhost:${port}`, 'Bootstrap');
 
   if (module.hot) {
     module.hot.accept();
