dc4eb867d488f202b606070ee334f51c92fd183d
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-10-06 23:20:02 +0900

    Add 'schema syncronize', 'schema drop' script

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,7 +2,7 @@ export default () => ({
   database: {
     type: 'mysql',
     host: process.env.DB_HOST || '',
-    port: parseInt(process.env.DB_PORT || ''),
+    port: parseInt(process.env.DB_PORT, 10) || '5432',
     user: process.env.DB_USER || '',
     name: process.env.DB_NAME || '',
     password: process.env.DB_PW || '',
