b64938f84e5399d1df646cc379b301e91a3639ae
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-10-06 21:10:19 +0900

    Add Logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 declare const module: any;
+const port = process.env.PORT || 8080;
+const host = process.env.HOST;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server is running on http://${host}:${port}`, 'Bootstrap');
 
   if (module.hot) {
     module.hot.accept();
