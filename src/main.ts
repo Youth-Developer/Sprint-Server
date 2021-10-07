98ef38d9bd73c0571bd791b3e89b65d1f0527c67
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-10-07 23:40:16 +0900

    migration script 수정 및 Swagger UI 생성

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
-const port = parseInt(process.env.PORT, 10) || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = parseInt(process.env.PORT, 10) || 8080;
+  const config = new DocumentBuilder()
+    .setTitle('Sprint')
+    .setDescription('Sprint 프로젝트를 위한 API 문서')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(`Server is running on http://localhost:${port}`, 'Bootstrap');
+  Logger.log(
+    `Swagger Document is running on http://localhost:${port}/api`,
+    'SwaggerDocument',
+  );
 
   if (module.hot) {
     module.hot.accept();
