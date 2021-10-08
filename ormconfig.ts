f4e478332325be06015dddbf75125547bd41b34a
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-10-08 15:34:27 +0900

    변수명 수정 및 ConnectionOption 변경

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,18 +1,20 @@
-import { ConnectionOptions } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-const config: ConnectionOptions = {
+const ormconfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  entities: [__dirname + 'entity/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migration/**/*{.ts,.js}'],
   cli: {
+    entitiesDir: 'src/entity',
     migrationsDir: 'src/migration',
   },
   synchronize: true,
   logging: true,
+  autoLoadEntities: true,
 };
-export = config;
+export = ormconfig;
