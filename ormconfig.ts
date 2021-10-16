bf69ccb9053c3ab384d0232055a48500bf460750
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-10-17 03:15:07 +0900

    :ambulance: (database.module.ts, ormconfig.ts) success db connection
    db connection을 성공하고, entity가 load되는 방법을 개선했습니다.

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,21 +1,23 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
 
-const ormconfig: TypeOrmModuleOptions = {
+const entity = []; // 여기에 entity를 넣어주세요
+dotenv.config();
+export const ormconfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_NAME,
-  entities: [join(__dirname + 'entity/**/*.entity{.ts,.js}')],
+  entities: entity,
   migrations: [join(__dirname + '/migration/**/*{.ts,.js}')],
   cli: {
-    entitiesDir: 'src/entity',
+    entitiesDir: 'src/entities',
     migrationsDir: 'src/migration',
   },
   synchronize: true,
   logging: true,
   autoLoadEntities: true,
 };
-export = ormconfig;
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import ormconfig from '../../ormconfig';
+import { ormconfig } from '../../ormconfig';
 
 @Module({ imports: [TypeOrmModule.forRoot(ormconfig)] })
 export class DatabaseModule {}
