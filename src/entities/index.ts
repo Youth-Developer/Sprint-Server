cf9bb4025c09815355a0ee18b08059d5be4f8c61
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-10-25 23:12:14 +0900

    :sparkles: (index.ts) QuestionPhoto Entity 추가

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,5 +1,6 @@
 import MemberEntity from './Member.entity';
 import QuestionEntity from './Question.entity';
 import AnswerEntity from './Answer.entity';
+import QuestionPhotoEntity from './QuestionPhoto.entity';
 
-export default [MemberEntity, QuestionEntity, AnswerEntity];
+export default [MemberEntity, QuestionEntity, AnswerEntity, QuestionPhotoEntity];
