c3d64e14416d74f674e298663d9b67ee550fc1c4
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-10-25 23:11:28 +0900

    :sparkles: (QuestionPhoto.entity.ts) QuestionPhoto Entity 생성

---
+++ src/entities/QuestionPhoto.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('question_photo')
+export default class QuestionPhotoEntity {
+  @PrimaryGeneratedColumn()
+  idx: number;
+
+  @Column({ name: 'image_path', nullable: true })
+  imagePath: string;
+}
