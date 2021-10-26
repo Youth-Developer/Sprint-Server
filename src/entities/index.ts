1b1f41b0066865fb2b4b24c5eb224987471323eb
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-10-26 16:20:00 +0900

    :art: (entities/index.ts) apply prettier

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -3,4 +3,10 @@ import QuestionEntity from './Question.entity';
 import AnswerEntity from './Answer.entity';
 import QuestionPhotoEntity from './QuestionPhoto.entity';
 import AnswerPhotoEntity from './AnswerPhoto.entity';
-export default [UserEntity, QuestionEntity, AnswerEntity, QuestionPhotoEntity, AnswerPhotoEntity];
+export default [
+  UserEntity,
+  QuestionEntity,
+  AnswerEntity,
+  QuestionPhotoEntity,
+  AnswerPhotoEntity,
+];
