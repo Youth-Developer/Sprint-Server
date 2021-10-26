266f9b67fd758bedc42eefddd60a3c978535a10f
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-10-26 16:01:57 +0900

    :triangular_flag_on_post: (user) member에서 user로 단어 교체
    BREAKING CHANGE: Change Member to User
    
    Closes #21

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,5 +1,5 @@
-import MemberEntity from './Member.entity';
+import UserEntity from './User.entity';
 import QuestionEntity from './Question.entity';
 import AnswerEntity from './Answer.entity';
 
-export default [MemberEntity, QuestionEntity, AnswerEntity];
+export default [UserEntity, QuestionEntity, AnswerEntity];
