5b8ce920c5d6e33ca92a5f50ea8aa401d954fbdb
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-11-01 10:00:57 +0900

    :dizzy: (Question.entity.ts) Question Entity 수정
    Question과 Answer가 상속관계를 이룰 때 연관관계를 매핑할 경우 Question이 자기참조를 하므로 Entity 생성 방식 변경

--- src/entities/Question.entity.ts
+++ src/entities/Question.entity.ts
@@ -1,8 +1,29 @@
-import { Column, Entity } from 'typeorm';
-import AnswerEntity from './Answer.entity';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('question')
-export default class Question extends AnswerEntity {
+export default class Question extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  idx: number;
+
   @Column({ nullable: false })
   title: string;
+
+  @Column({ nullable: false })
+  contents: string;
+
+  @Column({ nullable: false, default: 0 })
+  like: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
