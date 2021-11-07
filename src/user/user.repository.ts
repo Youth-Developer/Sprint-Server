e83846bd3fe515854a9246c230dde5a3abfdcb29
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-11-07 16:26:39 +0900

    :sparkles: (user) controller 생성 및 User custom Repository 생성

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,6 +1,7 @@
-import { Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import User from '../entities/User.entity';
 
+@EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async findUserById(idx: number): Promise<User | undefined> {
     return await this.findOne({ idx: idx });
