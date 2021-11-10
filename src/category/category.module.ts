78fc08712ab6193cc306b9187823d090cfbf5a0b
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-11-10 20:29:02 +0900

    :sparkles: (Category) Category Repository 생성

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryRepository } from './category.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoryRepository])],
   controllers: [CategoryController],
   providers: [CategoryService]
 })
