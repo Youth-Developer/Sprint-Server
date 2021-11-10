eac489d5769cd381894aad7295f6864132c0cdc8
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-11-10 11:54:40 +0900

    :fire: (user.repository) migration 'findUser' method to user.service

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,14 +2,4 @@ import { EntityRepository, Repository } from 'typeorm';
 import User from '../entities/User.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {
-  async findUserById(idx: number): Promise<User | undefined> {
-    return await this.findOne({ where: { idx: idx } });
-  }
-  async findUserByName(username: string): Promise<User | undefined> {
-    return await this.findOne({ where: { username: username } });
-  }
-  async findUserByEmail(email: string): Promise<User | undefined> {
-    return await this.findOne({ where: { email: email } });
-  }
-}
+export class UserRepository extends Repository<User> {}
