4df4638fe49cf2251edded2680765a29015be0c9
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-11-10 13:40:00 +0900

    :pencil: (swagger) register dto, user.controller

--- src/user/dto/register.dto.ts
+++ src/user/dto/register.dto.ts
@@ -5,18 +5,34 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
+  @ApiProperty({
+    example: 's20041@gsm.hs.kr',
+    description: '이메일',
+    required: true,
+  })
   @IsNotEmpty({ message: '이메일을 입력해주세요!' })
   @IsString()
   @Matches(/^[a-zA-Z0-9]+@gsm.hs.kr$/)
   readonly email!: string;
 
+  @ApiProperty({
+    example: '송유현',
+    description: '이름',
+    required: true,
+  })
   @IsNotEmpty({ message: '이름을 입력해주세요!' })
   @IsString()
   @MaxLength(10, { message: '닉네임이 너무 길어요!' })
   readonly username!: string;
 
+  @ApiProperty({
+    example: 'thddbgus1234',
+    description: '비밀번호',
+    required: true,
+  })
   @IsNotEmpty({ message: '비밀번호를 입력해주세요!' })
   @IsString()
   @MinLength(4, { message: '비밀번호가 너무 짧습니다!' })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterDto } from './dto/register.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: '회원가입' })
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   async signUp(@Body() registerDto: RegisterDto): Promise<void> {
