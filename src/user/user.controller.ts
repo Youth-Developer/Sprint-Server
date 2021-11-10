fa4b09d07c3bf6b2c87fad553870fde65e854ec9
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-11-10 12:01:07 +0900

    :sparkles: (user.controller) 회원가입 api 라우팅 및 Created HttpCode 반환

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  async signUp(@Body() registerDto: RegisterDto): Promise<void> {
+    return await this.userService.register(registerDto);
+  }
+}
