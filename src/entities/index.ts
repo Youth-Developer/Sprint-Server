135e225d0148e6b65c44cf84b85c0d958037d4b2
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-11-14 00:52:58 +0900

    :fire: (정비)

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,6 +1,6 @@
-import User from '../models/user/user.entity';
-import Question from '../models/question/question.entity';
-import Answer from '../models/answer/answer.entity';
-import QuestionPhoto from '../models/question-photo/questionPhoto.entity';
-import AnswerPhoto from '../models/answer-photo/answerPhoto.entity';
+import User from '../user/user.entity';
+import Question from '../question/question.entity';
+import Answer from '../answer/answer.entity';
+import QuestionPhoto from '../question-photo/questionPhoto.entity';
+import AnswerPhoto from '../answer-photo/answerPhoto.entity';
 export default [User, Question, Answer, QuestionPhoto, AnswerPhoto];
