cb5f2d502d1bc91da2e0b32fe125766440795968
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-11-25 18:01:34 +0900

    :recycle: (app.module, auth.module) module간의 불필요한 imports를 refactoring

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from '../user/user.repository';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRETE,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
