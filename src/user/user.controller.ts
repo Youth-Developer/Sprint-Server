4797a5b3725fbc3d4450e80e71072b007ddec7dd
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-11-25 18:06:14 +0900

    :sparkles: (user.controller.ts) add login router

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,33 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { ApiOperation } from '@nestjs/swagger';
+import { AuthService } from '../auth/auth.service';
+import { LocalAuthGuard } from '../auth/guards/local-auth.guard';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @ApiOperation({ summary: '회원가입' })
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   async signUp(@Body() registerDto: RegisterDto): Promise<void> {
-    return await this.userService.register(registerDto);
+    return await this.authService.register(registerDto);
+  }
+
+  @ApiOperation({ summary: '로그인' })
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Req() req) {
+    return this.authService.signToken(req.user);
   }
 }
