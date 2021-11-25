7f9ae53027579397490dc75d8dc61083cf00de31
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-11-25 18:21:19 +0900

    :recycle: (user.module.ts) import AuthModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { UserController } from './user.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([UserRepository]), AuthModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
