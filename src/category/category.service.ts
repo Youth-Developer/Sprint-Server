2de9411c2efe9f3e27c940317afb9b2005ed5211
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-09 14:23:31 +0900

    ✨(category.service) createCategory method

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CategoryRepository } from './category.repository';
+import Category from '../entities/Category.entity';
+import Question from '../entities/Question.entity';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    private readonly categoryRepository: CategoryRepository,
+  ) {}
+
+  async createCategory(question: Question, reqCategory: string[]) {
+    for(const postCategory of reqCategory){
+      const category: Category = new Category();
+      category.name = postCategory;
+      category.question = question;
+      await this.categoryRepository.save(category);
+    }
+  }
+
+}
