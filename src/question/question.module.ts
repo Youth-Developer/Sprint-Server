4538b2d3d6f39a807762fdd6cb51a8ab0ce7929f
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-09 14:07:15 +0900

    ✨(question.module) Question, User, Category Repository & Service imports

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from '../user/user.repository';
+import { QuestionRepository } from './quesiton.repository';
+import { CategoryRepository } from '../category/category.repository';
+import { CategoryService } from '../category/category.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([QuestionRepository, UserRepository, CategoryRepository])],
   controllers: [QuestionController],
-  providers: [QuestionService],
+  providers: [QuestionService, CategoryService],
 })
 export class QuestionModule {}
