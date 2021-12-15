2e9dcf3625094b96f69d6a7f9285e988bf02a902
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-12-15 11:06:30 +0900

    :sparkles: (TokenDto) class-validator 추가

--- src/auth/dto/token.dto.ts
+++ src/auth/dto/token.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class TokenDto {
+  @IsString()
   readonly username: string;
+
+  @IsString()
   readonly accessToken: string;
-  readonly refreshToken: string;
+
+  @IsString()
+  readonly refreshToken?: string;
 }
