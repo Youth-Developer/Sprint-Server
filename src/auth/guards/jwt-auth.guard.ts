75ed72d88224ef44e94ca9ae9fb1a3865aac9102
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-12-20 12:07:02 +0900

    :speech_balloon: (JwtAuthGuard) Invalid Exception에 대한 주석추가 및 Exception 추가

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -36,9 +36,11 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     } catch (e) {
       switch (e.message) {
         // 토큰에 대한 오류를 판단합니다.
-        case 'invalid signature':
-        case 'invalid token':
-        case 'jwt malformed':
+        case 'invalid signature': // signature의 구문을 분석할 수 없을 경우
+        case 'invalid token': // 헤더 또는 페이로드의 구문을 분석할 수 없을 경우
+        case 'jwt malformed': // 토큰에 구조적으로 문제가 생겼을 경우
+        case 'jwt signature is required': // 토큰에 signature을 확인하지 못 했을 경우
+        case 'Unexpected token': // 토큰의 형식이 원하는 형식과 맞지 않는 경우
           throw new UnauthorizedException('유효하지 않은 토큰입니다.');
 
         case 'jwt expired':
@@ -49,6 +51,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
           throw new GoneException('만료된 토큰입니다.');
 
         default:
+          console.log(e.message);
           throw new InternalServerErrorException('서버 오류입니다.');
       }
     }
