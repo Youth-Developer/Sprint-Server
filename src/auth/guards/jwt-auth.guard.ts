db6145547f22e58db1f8ccf8b0504f6c31383563
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-12-25 01:54:59 +0900

    :truck: (JwtAuthGuard) validateToken method를 TokenService로 migration

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,18 +1,14 @@
 import {
   BadRequestException,
   ExecutionContext,
-  GoneException,
   Injectable,
-  InternalServerErrorException,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { JwtPayload } from '../../common/interfaces/jwt-payload';
-import * as jwt from 'jsonwebtoken';
+import { TokenService } from '../../token/token.service';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor() {
+  constructor(private readonly tokenService: TokenService) {
     super();
   }
   canActivate(context: ExecutionContext) {
@@ -24,35 +20,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     }
 
     const accessToken = authorization.replace('Bearer ', '');
-    request.user = JwtAuthGuard.validateToken(accessToken); // request.user 객체에 디코딩된 토큰(유저 정보)을 저장합니다.
+    request.user = this.tokenService.validateToken(accessToken); // request.user 객체에 디코딩된 토큰(유저 정보)을 저장합니다.
     return true;
-  }
-
-  static validateToken(token: string, isRefresh = false): JwtPayload | boolean {
-    let verify: JwtPayload = null;
-    try {
-      const secretKey: string = process.env.JWT_ACCESS_TOKEN_SECRET;
-      verify = jwt.verify(token, secretKey) as JwtPayload;
-      return verify;
-    } catch (e) {
-      switch (e.message) {
-        // 토큰에 대한 오류를 판단합니다.
-        case 'invalid signature': // signature의 구문을 분석할 수 없을 경우
-        case 'invalid token': // 헤더 또는 페이로드의 구문을 분석할 수 없을 경우
-        case 'jwt malformed': // 토큰에 구조적으로 문제가 생겼을 경우
-        case 'jwt signature is required': // 토큰에 signature을 확인하지 못 했을 경우
-        case 'Unexpected token': // 토큰의 형식이 원하는 형식과 맞지 않는 경우
-          throw new UnauthorizedException('유효하지 않은 토큰입니다.');
-
-        case 'jwt expired': // 토큰이 만료되었을 경우
-          if (isRefresh) {
-            return true;
-          }
-          throw new GoneException('만료된 토큰입니다.');
-
-        default:
-          throw new InternalServerErrorException('서버 오류입니다.');
-      }
-    }
   }
 }
