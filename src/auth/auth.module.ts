874a631f2d45997c227367fbb041c8dc46f3f6c4
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-12-25 01:53:50 +0900

    :wrench: (AuthModule) jwt resource를 생성함에 따른 module 변경사항
    imports에서 JwtModule 삭제, providers에서 사용하지 않는 파일인 JwtStrategy와
    TokenService 삭제

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,26 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './strategies/jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from '../user/user.repository';
-import { TokenService } from '../token/token.service';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserRepository]),
-    PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_ACCESS_TOKEN_SECRET,
-      signOptions: {
-        algorithm: 'HS256',
-        expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME + 's',
-      },
-    }),
-  ],
-  providers: [AuthService, JwtStrategy, TokenService, LocalStrategy],
-  exports: [AuthService, TokenService],
+  imports: [TypeOrmModule.forFeature([UserRepository]), PassportModule],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
