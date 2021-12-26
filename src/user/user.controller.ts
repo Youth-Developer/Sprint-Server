1003f7cb0832b8e02001d17c64e6f79a61ef58ff
송유현 <68471917+songyouhyun@users.noreply.github.com>
2021-12-27 01:58:51 +0900

    :pencil2: (UserController, UserDecorator) import duplicate로 인한 이름 변경
    @User() => @UserDecorator()

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -21,7 +21,8 @@ import { SignInDto } from './dto/sign-in.dto';
 import { TokenDto } from '../token/dto/token.dto';
 import { TokenService } from '../token/token.service';
 import { LocalAuthGuard } from '../auth/guards/local-auth.guard';
-import { User } from '../common/decorators/user.decorator';
+import { UserDecorator } from '../common/decorators/user.decorator';
+import User from '../entities/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -59,7 +60,7 @@ export class UserController {
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
-  async login(@User() user) {
+  async login(@UserDecorator() user: User) {
     const data: TokenDto = await this.tokenService.createTokens(
       user.email,
       user.username,
@@ -68,6 +69,17 @@ export class UserController {
       status: 200,
       message: '로그인을 성공하였습니다.',
       data,
+    };
+  }
+
+  @ApiTags('Logout')
+  @ApiOperation({ summary: '로그아웃' })
+  @ApiOkResponse({ description: '로그아웃 성공(Refresh 토큰 삭제)' })
+  async logout(@UserDecorator() user: User) {
+    await this.authService.logout(user.idx);
+    return {
+      status: 200,
+      message: '로그아웃을 성공하였습니다.',
     };
   }
 }
