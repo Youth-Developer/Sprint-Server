67a337ee76c0c7f71fb5a0a3bb8e709b575dba7e
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-27 09:34:32 +0900

    :sparkles: (QuestionRepository) add index router
    게시글 전체조회 router 추가

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { QuestionService } from './question.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { QuestionsDto } from './dto/find.dto';
 
 @Controller('question')
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
+
+  @ApiTags('전체조회')
+  @ApiOperation({ summary: '전체조회' })
+  @Get('/')
+  async index() {
+    const data: QuestionsDto[] = await this.questionService.findAllQuestion();
+    return {
+      status: 200,
+      message: "전체조회에 성공하였습니다",
+      data
+    }
+  }
 }
--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QuestionRepository } from './quesiton.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([QuestionRepository])],
   controllers: [QuestionController],
   providers: [QuestionService],
 })
