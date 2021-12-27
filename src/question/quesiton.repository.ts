ebd33ffcc387f771c3649a7d83c6ebc3529a804f
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-27 09:30:04 +0900

    :sparkles: (QuestionRepository) add countAnswer method
    댓글 수를 조회하는 method 추가

--- src/question/quesiton.repository.ts
+++ src/question/quesiton.repository.ts
@@ -1,5 +1,20 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getConnection, getRepository, Repository } from 'typeorm';
 import Question from '../entities/question.entity';
+import Answer from '../entities/answer.entity';
 
 @EntityRepository(Question)
-export class QuestionRepository extends Repository<Question> {}
+export class QuestionRepository extends Repository<Question> {
+  async countAnswer() {
+   return  await getConnection()
+      .createQueryBuilder()
+      .select()
+      .addSelect(subQuery => {
+        return subQuery
+          .select("count(*)")
+          .from(Answer, "a")
+          .where("a.question_idx = q.question_idx")
+      }, "name")
+      .from(Question, "q")
+     .execute()
+  }
+}
