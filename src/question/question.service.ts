4631e55bb8ce387c178155ab721c2944d8c4c65b
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-27 09:36:57 +0900

    :sparkles: (QuestionService) add findAllQuestion method
    게시글 전체조회 기능 구현

--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { QuestionRepository } from './quesiton.repository';
+import Question from '../entities/question.entity';
+import { QuestionsDto } from './dto/find.dto';
+import { CountDto } from './dto/count.dto';
 
 @Injectable()
-export class QuestionService {}
+export class QuestionService {
+  constructor(private readonly questionRepository: QuestionRepository) {}
+
+  async findAllQuestion()  {
+    const question: Question[] = await this.questionRepository.find({ relations: ["category"]});
+    const count: CountDto[] = await this.questionRepository.countAnswer();
+    return await this.convertQuestions(question, count);
+  }
+
+  async convertQuestions(questions: Question[], count: CountDto[]): Promise<QuestionsDto[]> {
+    const questionsDto = new QuestionsDto();
+    const result: QuestionsDto[] = [];
+    const name: string[] = [];
+
+    questions.forEach(function (question, index) {
+      const { contents, title, createdAt, like } = question;
+      question.category.forEach(function (category) {
+        name.push(category.name);
+      });
+      questionsDto.title = title;
+      questionsDto.contents = contents;
+      questionsDto.count = count[index].name;
+      questionsDto.like = like;
+      questionsDto.createdAt = createdAt;
+      questionsDto.category = name;
+      result.push(questionsDto);
+    });
+    return result;
+  }
+}
