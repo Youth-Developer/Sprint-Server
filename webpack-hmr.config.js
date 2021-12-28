d088e2911c454f4180aa479d44385facb50ed36e
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-28 09:36:00 +0900

    :dizzy: (webpack-hmr.config) 패키지 변경에 따른 설정 변경

--- webpack-hmr.config.js
+++ webpack-hmr.config.js
@@ -1,13 +1,14 @@
-const nodeExternals = require('webpack-node-externals');
+const pnpExternals = require('webpack-pnp-externals');
 const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+const { WebpackPnpExternals } = require('webpack-pnp-externals');
 
 module.exports = function (options, webpack) {
   return {
     ...options,
     entry: ['webpack/hot/poll?100', options.entry],
     externals: [
-      nodeExternals({
-        allowlist: ['webpack/hot/poll?100'],
+      WebpackPnpExternals({
+        exclude: ['webpack/hot/poll?100'],
       }),
     ],
     plugins: [
